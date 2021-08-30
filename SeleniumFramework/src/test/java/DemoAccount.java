17e151f3bf0ec8a1f06d14ed0fea80017477b553
anmendeza <andrea@firstfactory.com>
2021-08-29 21:25:30 -0600

    tercera semana segundo push

--- SeleniumFramework/src/test/java/DemoAccount.java
+++ SeleniumFramework/src/test/java/DemoAccount.java
@@ -1,2 +1,81 @@
-package PACKAGE_NAME;public class DemoAccount {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+public class DemoAccount {
+//This will save us of having a chrome driver.exe
+    @BeforeTest
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+    }
+    @Ignore
+    @Test
+    public void test_capabilities(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1700,800");  //this makes the browser window to be in a certain size
+        options.addArguments("--headless");     //This run the test but it never opens up the browser
+        options.setHeadless(true);      //This does the same thing than the one above ^^^
+        options.setAcceptInsecureCerts(true);    //This allows to go to pages that notify the connetion is not private
+
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.seleniumeasy.com/test/");
+
+        driver.manage().window().maximize();    //maximizes the browser windows
+
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void test_waits(){
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html");
+        driver.findElement(By.id("downloadButton")).click();
+
+        boolean result = false;
+        //exeption handlers
+        try{
+            result = wait.until(ExpectedConditions.textToBe(By.className("progress-label"), "Complete!"));
+        }
+        catch (WebDriverException exeption){
+            System.out.println("It didn't work");
+        }
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void drag_and_drop(){        //drag and drop practice
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.seleniumeasy.com/test/drag-and-drop-demo.html");
+
+        Actions actions = new Actions(driver);
+
+        WebElement box1 = driver.findElement(By.xpath("//span[text()='Draggable 1']"));
+        WebElement drop = driver.findElement(By.id("mydropzone"));
+        Assert.assertTrue(drop.isDisplayed());
+
+        Point punto = drop.getLocation();
+
+        actions.dragAndDrop(box1, drop).release().build().perform();
+        actions.dragAndDropBy(box1, punto.getX(), punto.getY()).release().build().perform();
+
+        actions.perform();
+        WebElement dropped = driver.findElement(By.xpath("//div[@id='droppedlist']/span[text() = 'Draggable 1']"));
+        Assert.assertTrue(dropped.isDisplayed());
+
+    }
 }
--- SeleniumFramework/src/test/java/TestAccount.java
+++ SeleniumFramework/src/test/java/TestAccount.java
@@ -1,2 +1,91 @@
-package PACKAGE_NAME;public class TestAccount {
+import com.sun.org.glassfish.gmbal.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+public class TestAccount {
+    //@Description("Validate test login was successful")
+    @Test //for this, the credentials are registered, so the test will pass
+    public void Test_Login_Successful(){
+        String username = "andrea@mailnator.com";
+        String password = "qwrt1234";
+
+        String pathToDriver = Test.class.getResource("/chromedriver.exe").getPath();
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
+                                                    //These 3 lines are setting the driver
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.opencart.com/");  //This is telling the driver where to go
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        //This goes to login page
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
+        driver.findElement(By.linkText("Login")).click();
+
+        //This fills the form
+        driver.findElement(By.name("email")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("[value='Login']")).click();
+
+        //This asserts the account is logged in
+        WebElement logOutButton = driver.findElement(By.linkText("Logout"));
+        Assert.assertTrue(logOutButton.isDisplayed());
+
+        //This takes a screenshot
+        //TakeScreenshot(driver);
+
+        //This closes the browsers and ends the execution
+        driver.close();
+        driver.quit();
+    }
+
+    //@Description("Validate that the login is working with non valid credentials")
+    @Test //for this, the password is not correct, so the test will fail
+    public void Test_Login_Unsuccessful(){
+        String username = "andrea@mailnator.com";
+        String password = "t1234";
+        String expectedMessage = "Warning: No match for E-Mail Address and/or Password";
+
+        String pathToDriver = Test.class.getResource("/chromedriver.exe").getPath();
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
+        //These 3 lines are setting the driver
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.opencart.com/");  //This is telling the driver where to go
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        //This goes to login page
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
+        driver.findElement(By.linkText("Login")).click();
+
+        //This fills the form
+        driver.findElement(By.name("email")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("[value='Login']")).click();
+
+        //This asserts the account is logged in
+        WebElement alertMessage = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
+       Assert.assertEquals(expectedMessage.toLowerCase(), alertMessage.getText().toLowerCase().trim());
+       //what trim does is taking the spaces from the beginning and the end of the string
+
+        //This takes a screenshot
+        //TakeScreenshot(driver);
+
+        //This closes the browsers and ends the execution
+        driver.close();
+        driver.quit();
+
+
+        /*@Attachment(value = "screenshot", type = "image/png")
+        public byte[] TakeScreenshot(WebDriver driver){
+            return ((TakeScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        }
+         */
+    }
 }
+//Note: remember to always download the chrome driver and add it to the resources folder. if this step is not made, it will not run
